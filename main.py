78091095f1f7ef62240243276f788db3f650ed96
ctrevai <ctrevai@gmail.com>
2024-04-22 07:23:46 +0000

    add dockerignore

--- main.py
+++ main.py
@@ -10,42 +10,40 @@ PRODUCT_DB = ["shaver", "egg", "cola"]
 
 
 @app.get("/")
-def root():
+async def root():
     return {"message": "Welcome to Inventory API"}
 
 # /list-products
 
 
 @app.get("/list-products")
-def list_products():
+async def list_products():
     return {"products": PRODUCT_DB}
 
 # /product-by-index
 
 
 @app.get("/product-by-index")
-def product_by_index(index):
+async def product_by_index(index):
     return {"product": PRODUCT_DB[index]}
 
 # /get-random-product
 
 
 @app.get("/get-random-product")
-def get_random_product():
+async def get_random_product():
     return {"product": random.choice(PRODUCT_DB)}
 
 # /add-product
 
 
 @app.post("/add-product")
-def add_product(product):
+async def add_product(product):
     return {"message": "Add product"}
 
 # /get-product?id={id}
 
 
-app.get("/get-product")
-
-
-def get_product(id):
+@app.get("/get-product")
+async def get_product(id):
     return {"product": PRODUCT_DB[id]}
