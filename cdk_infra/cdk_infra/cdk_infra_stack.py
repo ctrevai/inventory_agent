3722e7c36f387250801ed3b415e5516e8721e68a
ctrevai <ctrevai@gmail.com>
2024-04-27 05:31:49 +0000

    add apigateway

--- cdk_infra/cdk_infra/cdk_infra_stack.py
+++ cdk_infra/cdk_infra/cdk_infra_stack.py
@@ -2,8 +2,10 @@ from aws_cdk import (
     Duration,
     Stack,
     aws_lambda as _lambda,
+    aws_apigateway as apigateway
 )
 from constructs import Construct
+
 
 class CdkInfraStack(Stack):
 
@@ -11,11 +13,22 @@ class CdkInfraStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         self.build_lambda_func()
+        self.build_api_gateway()
 
     def build_lambda_func(self):
         self.docker_lambda = _lambda.DockerImageFunction(
             self, id="inventoryapi",
-            function_name="inventoryapi",code=_lambda.DockerImageCode.from_image_asset("../image"),
+            function_name="inventoryapi", code=_lambda.DockerImageCode.from_image_asset("../image"),
             memory_size=1024,
             timeout=Duration.seconds(300)
         )
+
+    # add api gateway
+    def build_api_gateway(self):
+        self.api_gateway = apigateway.LambdaRestApi(
+            self, "InventoryApi",
+            handler=self.docker_lambda,
+            proxy=False
+        )
+        self.api_gateway.root.add_method(
+            "GET", apigateway.LambdaIntegration(self.docker_lambda))
